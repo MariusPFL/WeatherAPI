9d4bd089da9d30132b2cb9c96c497697cb618c9d
Marius Pfluger <marius.pfluger@autismuslink.ch>
2022-12-14 14:12:37 +0100

    Delete Class JSON Converter

--- WeatherAPI/Model/WeatherForecast.cs
+++ WeatherAPI/Model/WeatherForecast.cs
@@ -10,7 +10,6 @@
     public partial class WeatherForecast
     {
         [JsonProperty("cod")]
-        [JsonConverter(typeof(ParseStringConverter))]
         public long Cod { get; set; }
 
         [JsonProperty("message")]
@@ -177,35 +176,4 @@
 
     public enum MainEnum { Clouds, Rain, Snow, Clear };
 
-
-    internal class ParseStringConverter : JsonConverter
-    {
-        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
-
-        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
-        {
-            if (reader.TokenType == JsonToken.Null) return null;
-            var value = serializer.Deserialize<string>(reader);
-            long l;
-            if (Int64.TryParse(value, out l))
-            {
-                return l;
-            }
-            throw new Exception("Cannot unmarshal type long");
-        }
-
-        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
-        {
-            if (untypedValue == null)
-            {
-                serializer.Serialize(writer, null);
-                return;
-            }
-            var value = (long)untypedValue;
-            serializer.Serialize(writer, value.ToString());
-            return;
-        }
-
-        public static readonly ParseStringConverter Singleton = new();
-    }
 }
